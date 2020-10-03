19a07d1d841467c8509b888464602b8b1e5351b8
Tauhoo <tauhoo_ice@hotmail.com>
2020-10-03 20:41:24 +0700

    feat: add year key to the projects and configuration

--- build.js
+++ build.js
@@ -1,37 +1,61 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
 
-const excludes = [".git", ".idea", "node_modules"];
-const directories = [];
+const excludes = ['.git', '.idea', 'node_modules']
+const directories = []
+
+const { years } = require('./config')
 
 fs.readdirSync(path.join(__dirname)).forEach(file => {
-    if (fs.lstatSync(path.join(__dirname, file)).isDirectory()) {
-        if (!excludes.includes(file)) {
-            directories.push(file);
-        }
+  if (fs.lstatSync(path.join(__dirname, file)).isDirectory()) {
+    if (!excludes.includes(file)) {
+      directories.push(file)
     }
-});
+  }
+})
 
-let gameTemplate = ``;
+let gameTemplateMap = {}
+
+for (const { year, header, description } of years) {
+  gameTemplateMap[year] = `
+    <div>
+        <h1 class="heading">${header}</h1>
+        <p class="desc">${description}</p>
+        <a
+        class="hacking"
+        href="https://github.com/chunza2542/noobtoberfest"
+        target="_blank"
+        >START HACKING</a>
+    </div>
+    <br />
+    <hr />
+    `
+}
 
 directories.forEach(directory => {
-    let info = fs.readFileSync(path.join(__dirname, directory, "info.json"));
-    info = JSON.parse(info.toString());
+  let info = fs.readFileSync(path.join(__dirname, directory, 'info.json'))
+  info = JSON.parse(info.toString())
 
-    const template = `
+  const template = `
         <div class="game">
             <a class="head" href="/${directory}" target="_blank"><h3>${info.name}</h3></a>
             <p style="color: #ccc">${info.description}</p>
             <a href="${info.facebook}" target="_blank" class="author">${info.author}</a>
         </div>`
 
-    gameTemplate += template;
-});
+  gameTemplateMap[info.year] += template
+})
 
-let htmlTemplate = fs.readFileSync(path.join(__dirname, "template.html")).toString();
-htmlTemplate = htmlTemplate.replace("#include-template", gameTemplate);
+const totalTemplate = years
+  .map(({ year }) => gameTemplateMap[year])
 
-fs.writeFileSync("index.html", htmlTemplate);
-console.log("Done~");
+  .join()
 
+let htmlTemplate = fs
+  .readFileSync(path.join(__dirname, 'template.html'))
+  .toString()
 
+htmlTemplate = htmlTemplate.replace('#include-template', totalTemplate)
+
+fs.writeFileSync('index.html', htmlTemplate)
+console.log('Done~')
---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  years: [
+    {
+      year: 2020,
+      header: 'Noobtoberfest 2020',
+      description: `Game-coding challenge for everyone who wants to contribute to an open-source project for Hacktoberfest 2020`,
+    },
+    {
+      year: 2019,
+      header: 'Noobtoberfest 2019',
+      description: `Game-coding challenge for everyone who wants to contribute to an open-source project for Hacktoberfest 2019`,
+    },
+  ],
+}
