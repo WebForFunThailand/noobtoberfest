99f62a34b6e8ec72ab6657d151b802ce1002aee6
Chun Rapeepat <chunza2542@gmail.com>
2020-10-06 12:06:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  title: 'Noobtoberfest 2020',
+  title: 'Noobtoberfest 2020 | Web For Fun Hackathon Thailand',
   banner: {
     title: 'Noobtoberfest !!!',
-    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+    description: `Game-coding challenge for everyone who wants to contribute to an open-source project for Hacktoberfest 2020. The main objective of this challenge is to help everyone get started with contributing to open-source projects and we hope you enjoy coding and learning along the way during the Hacktoberfest 2020 period.`,
   },
   years: [
     {
       year: 2020,
       header: '2020',
-      description: `Game-coding challenge for everyone who wants to contribute to an open-source project for Hacktoberfest 2020`,
+      description: `Game-coding challenge for everyone who wants to contribute to an open-source project for Hacktoberfest 2020 organized by <a target="_blank" style="color: #2d6bef; text-decoration: none; cursor:pointer;">Web For Fun Thailand</a>`,
     },
     {
       year: 2019,
