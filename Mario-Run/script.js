57f586f0c0085e094282b51cb537fc8d57ec0709
Abhijay Jain <abhijay007j@gmail.com>
2020-10-14 16:42:29 +0530

    Update script.js
    added more playable functionality

--- Mario-Run/script.js
+++ Mario-Run/script.js
@@ -1,7 +1,25 @@
 var character = document.getElementById("character");
 var block = document.getElementById("block");
+var highestScore = document.getElementById('highest-score');
+var score = document.getElementById('score');
+var highestScorePoint = Number(localStorage.getItem('mario-highest-score')) || 0;
+var currentPoint = 0;
+
+checkHighestScore();
+
+function keysControl(e){
+    if(e.keyCode == 32 || e.keyCode == 38){
+        jump()
+    }
+}
+
+document.body.addEventListener("keyup", keysControl);
+document.body.addEventListener("click", jump);
 
 function jump() {
+    const audio = new Audio('sounds/jump.mp3');
+    audio.play();
+    
     if (character.classList != "animate") {
         character.classList.add("animate");
     }
@@ -12,6 +30,11 @@ function jump() {
     }, 1000);
 }
 
+var incrementScore = setInterval(function () {
+    currentPoint++;
+    score.innerHTML = currentPoint;
+}, 100);
+
 var checkLost = setInterval(function () {
     var characterTop =
         parseInt(window.getComputedStyle(character).getPropertyValue("top"));
@@ -19,9 +42,33 @@ var checkLost = setInterval(function () {
         parseInt(window.getComputedStyle(block).getPropertyValue("left"));
 
     if (blockLeft < 350 && blockLeft > 300 && characterTop >= 150) {
-
+        gameOver.style.display = "block";
+        character.style.display = "none";
         block.style.animation = "none";
         block.style.display = "none";
-        alert("Your Lose -GameOver.");
+        alert("Your Lose - GameOver");
+        var newGame = confirm("Start new Game?");
+        if (newGame) {
+            location.reload();
+        }else{
+            character.style.animation = "none";
+            character.style.display = "none";
+            document.body.removeEventListener("keyup", keysControl);
+            document.body.removeEventListener("click", jump)
+        }
+        clearInterval(incrementScore);
+        checkNewRecord();
     }
 }, 10);
+
+function checkHighestScore () {
+    if (highestScore) {
+        highestScore.innerHTML = highestScorePoint;
+    }
+}
+
+function checkNewRecord () {
+    if (currentPoint > highestScorePoint) {
+        localStorage.setItem('mario-highest-score', currentPoint);
+    }
+}
